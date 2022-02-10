7f5c4f19dcda8a6407b21ea5420b6154b6266412
Roman <roman.quistler@gmail.com>
2022-02-10 22:23:49 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ done
 
 echo "Deploying target: $target"
 
-echo "compose: $(whereis docker-compose)"
+echo "compose: $(whereis docker ps -a)"
 
 #docker-compose build --no-cache --build-arg ENVPROFILE=$target
 docker-compose build --build-arg ENVPROFILE=$target
